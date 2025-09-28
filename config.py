d283c064e72c4c9d4a0999480cf5e4ec32ac8d55
IPPO34 <carlostristan154@gmail.com>
2025-09-28 16:06:45 -0500

    Actualizado

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = "postgresql://base_de_datos_bigplast_user:ltFDyaG6SNDBSuSqMCzNTjmavSjjVW5P@dpg-d3cpm1j7mgec73ao7jtg-a.oregon-postgres.render.com/base_de_datos_bigplast"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET = os.getenv("JWT_SECRET", "change_me")
     JWT_EXP_DAYS = int(os.getenv("JWT_EXP_DAYS", "7"))
